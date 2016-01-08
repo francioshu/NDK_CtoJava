fcd46b08964aae2ee1204f64dd1e6680ec827687
francioshu <shufu.shu@gamil.com>
2016-01-08 10:17:37 +0800

    merge coee
    merge coee

---
+++ obj/local/armeabi/objs/NDK_04/CToJava.o.d
@@ -0,0 +1,23 @@
+/cygdrive/i/JavaWorkSpace/Blog/NDK_04/obj/local/armeabi/objs/NDK_04/CToJava.o: \
+ /cygdrive/i/JavaWorkSpace/Blog/NDK_04/jni/CToJava.c \
+ /cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/string.h \
+ /cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/malloc.h \
+ /cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/android/log.h \
+ /cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/jni.h \
+ /cygdrive/i/JavaWorkSpace/Blog/NDK_04/jni/Provider.h
+
+/cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/string.h:
+
+/cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/malloc.h:
+
+/cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/android/log.h:
+
+/cygdrive/e/Java/android-ndk-r5/platforms/android-5/arch-arm/usr/include/jni.h:
+
+/cygdrive/i/JavaWorkSpace/Blog/NDK_04/jni/Provider.h:
